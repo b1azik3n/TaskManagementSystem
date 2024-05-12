f14e363689fc4be2e215ebb25f902aaf4a3e0536
Bikram Shrestha <stha.bikram999@gmail.com>
2024-05-12 19:22:31 +0545

    init

--- DomainLayer/obj/Debug/net6.0/DomainLayer.AssemblyInfo.cs
+++ DomainLayer/obj/Debug/net6.0/DomainLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DomainLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f7a05a965b6fd31684488fae16c8040fca87b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DomainLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DomainLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
